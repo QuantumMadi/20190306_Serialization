37978fc76608bef5e481e28b6ddd101db4724f0d
Madi <45874448+QuantumMadi@users.noreply.github.com>
2019-03-11 23:35:17 +0600

    attribute

---
+++ Theory/Book/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApp
+{
+    [Serializable]
+    public class Book
+    {
+        public string Name{ get;set;}
+        public int Price { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishYear { get; set; }
+        
+    }
+}
---
+++ Theory/Book/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace BookApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> array = new List<Book>
+            {
+                new Book()
+                {
+                    Name = "War and peace",
+                    Author = "Tolstoy",
+                    Price = 1000,
+                    PublishYear = new DateTime(1996,05,06),
+                },
+                 new Book()
+                {
+                    Name = "Monte Cristo",
+                    Author = "A.Duma",
+                    Price = 1500,
+                    PublishYear = new DateTime(1945,07,06),
+                },
+                  new Book()
+                {
+                    Name = "Prestuplenie i nakazanie",
+                    Author = "Dostoevsky",
+                    Price = 2000,
+                    PublishYear = new DateTime(1925,07,08),
+                }
+            };
+
+            SaveAsBinaryFormat(array, "Books.dat");
+            foreach(var book in LoadFromBinaryFile("Books.dat"))
+            {
+                Console.WriteLine($"{book.Name}\n{book.Author}\n{book.Price}\n{book.PublishYear.Year}\n-------------------");
+
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+
+        static void SaveAsBinaryFormat(List<Book>objects,string fileName )
+        {
+            BinaryFormatter binFormat = new BinaryFormatter();
+            using(var fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                binFormat.Serialize(fStream, objects);
+            }
+        }
+        static List<Book> LoadFromBinaryFile(string fileName)
+        {
+            List<Book> books;
+            BinaryFormatter binFormat = new BinaryFormatter();
+            using(var fs = File.OpenRead(fileName))
+            {
+               
+                books = (List<Book>)binFormat.Deserialize(fs);
+            }
+
+            return books;
+        }
+    }
+}
